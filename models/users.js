1dadfdc9e4c9364c1895738051a756c6e84142d6
anakellyc <anakcampos@gmail.com>
2018-11-26 10:16:48 +0100

    Updated

---
+++ models/users.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+function capitalize (val) {
+  if (typeof val !== 'string') {
+      val = '';
+  }
+  return val.charAt(0).toUpperCase() + val.substring(1);
+}
+
+const userSchema = new Schema({
+  firstName: {Type: String, required: true, set: capitalize},
+  lastName: {Type: String, required: true, set: capitalize},
+  email    : { type: String},
+  username : { type: String},
+  password: {type: String, minlength: 8},
+  avatarUrl: { type: String, default: 'images/default-avatar.png' }
+});
+
+const User = mongoose.model('User', catSchema);
+module.exports = User;
