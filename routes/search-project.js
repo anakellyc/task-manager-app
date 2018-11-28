7fbcb0b472c9601b95c537eed9ff18c9b8659693
Amel Ramirez <amelramirez@Amels-MacBook-Air.local>
2018-11-28 19:37:31 +0100

    mandioca

--- routes/search-project.js
+++ routes/search-project.js
@@ -1,6 +1,7 @@
 var express = require("express")
 var app = express()
 
+const Project = require("../models/projects.js")
 
 app.get('/', function(req, res) {
   if (req.signedCookies.loggedIn == "true") {
@@ -11,4 +12,48 @@ app.get('/', function(req, res) {
   }
 })
 
+
+app.post('/', function(req, res) {
+   
+  Project.find( { projectName: { $regex: req.body.search, $options:'i'}})
+  .then((result) =>{
+    
+      // console.log(result)
+      if (req.signedCookies.loggedIn == "true") {
+        res.render('search-project', {result: result, loggedIn:true})
+      }
+      else {
+        res.render('search-project', {result: result, loggedIn:false})
+      }
+      
+  })
+  .catch(err =>{
+    throw err;
+  })
+})
+
+
+///////////////////////////
+var projectToAdd;
+//button event {
+let userId = req.signedCookies.userId
+
+User.findOneAndUpdate({'_id': userId}, {"$push": {"projects": {projectName: projectToAdd.projectName, projectToAdd: newproject.id}}})
+  console.log("this result is?", result)
+  .then((result) =>{
+    projectToAdd = result
+    console.log(result)
+    if (req.signedCookies.loggedIn == "true") {
+      res.render('search-project', {result: result, loggedIn:true})
+    }
+    else {
+      res.render('search-project', {result: result, loggedIn:false})
+    }
+    
+})
+
+//}
+/////////////////////////////
 module.exports = app
+
+
